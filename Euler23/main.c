26eba85cc5a1a56ea1189d934d4dcf5a7aa9de6c
Pedro Pinto <p.pinto@fe.up.pt>
2020-08-07 03:16:33 +0100

    Changes algorithm for 23 (bugged for now)

--- Euler23/main.c
+++ Euler23/main.c
@@ -1,28 +1,40 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <stdlib.h>
 
 static inline bool is_abundant(unsigned int n);
 
-unsigned int sum_divisors(unsigned int n);
+static unsigned int sum_divisors(unsigned int n);
 
-bool is_sum_of_abundants(unsigned int N, const unsigned int *abundants, unsigned int num_abundants);
+static void find_soa(
+        const unsigned int *abundants,
+        unsigned int num_abundants,
+        bool *soa_map);
 
-/**
- *
- * 1) find all abundant numbers from 12 to 28123
- * 2) iterate from N=1 to 28123 and find if each number can be a sum of abundants
- *      iterate the list of abundants and subtract from from N, checking if the difference if abundant
- *      if it is, N can be expressed as a sum of two abundants
- *      it it's not, sum
- *
- *
- * @return
- */
+static unsigned int find_abundants(unsigned int *abundants);
+
+static unsigned int sum_non_soa(const bool *soa_map);
+
 int main() {
 
-    // sorted list of abundants
+    // 1) find abundants
     unsigned int abundants[7000];
+    unsigned int num_abundants = find_abundants(abundants);
+
+    // 2) find all sums of abundants (soa) up to 28123
+    // this maps each number to whether it is a soa
+    bool soa_map[28124] = {false};
+    find_soa(abundants, num_abundants, soa_map);
+
+    // 3) iterate up to 28123 and sum non-soa
+    unsigned int sum = sum_non_soa(soa_map);
+
+    printf("answer:   %d\nsolution: %d\n", sum, 4179871);
+
+    return 0;
+}
+
+static unsigned int find_abundants(unsigned int *abundants) {
+
     unsigned int num_abundants = 0;
 
     for (unsigned int i = 12; i < 28123; ++i) {
@@ -32,41 +44,53 @@ int main() {
         }
     }
 
+    return num_abundants;
+}
+
+static void find_soa(
+        const unsigned int *abundants,
+        const unsigned int num_abundants,
+        bool *soa_map) {
+
+    for (unsigned int i = 0; i < num_abundants - 1; ++i) {
+
+        const unsigned int a1 = abundants[i];
+
+        for (unsigned int j = i + 1; j < num_abundants; ++j) {
+
+            const unsigned int a2 = abundants[j];
+            const unsigned int soa = a1 + a2;
+
+            if (soa > 28123) {
+
+                break;
+            }
+
+            soa_map[soa] = true;
+        }
+    }
+}
+
+static unsigned int sum_non_soa(const bool *soa_map) {
+
     unsigned int sum = 0;
-    for (unsigned int N = 1; N <= 28123; ++N) {
 
-        if(!is_sum_of_abundants(N, abundants, num_abundants)) {
-            sum += N;
+    for (unsigned int i = 0; i <= 28123; ++i) {
+
+        if (soa_map[i] != 1) {
+            sum += i;
         }
     }
 
-    printf("answer: %d\n", sum);
-
-    return 0;
+    return sum;
 }
 
-bool is_sum_of_abundants(unsigned int N, const unsigned int *abundants, unsigned int num_abundants) {
+static bool is_abundant(const unsigned int n) {
 
-    for (unsigned int a = 0; a < num_abundants; ++a) {
-
-        const unsigned int abundant = abundants[a];
-
-        if (N <= abundant) {
-            return false;
-        }
-
-        unsigned int diff = N - abundant;
-        if (is_abundant(diff)) {
-
-//            printf("%d = %d + %d\n", N, abundant, diff);
-            return true;
-        }
-    }
-
-    return false;
+    return sum_divisors(n) > n;
 }
 
-unsigned int sum_divisors(unsigned int n) {
+static unsigned int sum_divisors(const unsigned int n) {
 
     // 1 is always a divisor, so we start with sum=1 and the loop at 2
     unsigned int sum = 1;
@@ -81,9 +105,4 @@ unsigned int sum_divisors(unsigned int n) {
     }
 
     return sum;
-}
-
-static inline bool is_abundant(unsigned int n) {
-
-    return sum_divisors(n) > n;
 }
